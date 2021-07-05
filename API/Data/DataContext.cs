462930ea22edbd96f38c6940d1d0e7fc438a517b
Michael John Gatab <michael.gatab@mytikasllc.com>
2021-07-05 22:27:53 +0800

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -8,8 +8,28 @@ namespace API.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
+
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.LikeUserId});
+            
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikeByUsers)
+                .HasForeignKey(s => s.LikeUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- API/Data/Migrations/20210705051031_EntityAdded.Designer.cs
+++ API/Data/Migrations/20210705051031_EntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210705051031_EntityAdded")]
+    partial class EntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -103,7 +105,7 @@ namespace API.Data.Migrations
 
                     b.HasIndex("LikeUserId");
 
-                    b.ToTable("Likes");
+                    b.ToTable("Like");
                 });
 
             modelBuilder.Entity("API.Entities.Photo", b =>
